3dc81a1dd2536cfecc94aef6744a4ba9756ac6fb
sathvik-kvs <sathviksaikvs1234@gmail.com>
2025-04-19 15:53:27 +0530

    1st project added

---
+++ Beginner/1_Band_Name_Genetator/name_generator.py
@@ -0,0 +1,4 @@
+city_name = input("Enter the name of the city : ")
+pet_name = input("Enter the name of your pet : ")
+print("############ NAME GENERATED #############")
+print(city_name , pet_name)
